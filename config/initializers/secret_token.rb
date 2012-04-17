6e4101832699dd1e9f83e06bd8a00a49fccb3b33
Bruno Torres <bruno.torres@sv.cmu.edu>
2012-04-16 21:51:52 -0700

    placeholder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalSite2::Application.config.secret_token = 'd31d7ca0a0135d487e17bd821d90ee20222aa33fe111fb133b8c06f5ebf9ed121b81a5c2996deda6914f2b52c40d5aca45ea23316aada9b7e4c158754bf68296'
